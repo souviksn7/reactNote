9524d22b7aa3022965950073593559457a1002dc
Souvik Nandi <souviknandi235@gmail.com>
2022-01-18 00:39:29 +0530

    your_commit_message

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./Header";
+// import Footer from "./Footer";
+// import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <Note />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+// import React from 'react';
+// import Header from './Header';
+// function App(){
+//     return <div>
+//         <Header />
+//     </div>
+// }
+// export default App;
